654ef0fed4f3a400ea5dbb5a7f2cc83adc3d85e0
Dylan Curzon <hyperberry@curz.sh>
2026-06-30 14:27:30 +0100

    nix: feat(www/dcurgz.me): use EB Garamond for body text

--- nix/modules/www/dcurgz.me/webroot/default.nix
+++ nix/modules/www/dcurgz.me/webroot/default.nix
@@ -63,6 +63,12 @@ in
             path = page.src;
           }) (builtins.filter (x: x.permalink != null) cfg.pages);
           resources = [
+            (let
+              ttf_EB_Garamond = pkgs.fetchurl {
+                url = "https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap";
+                hash = "sha256-QmSjvJ0CacqZTN8FQ9/Zp+gO1K5ken116CHMBAYfZ38=";
+              };
+            in
             {
               name = "style.css";
               path = (stdenv.mkDerivation {
@@ -70,12 +76,13 @@ in
                 src = ./stylesheets;
                 buildPhase = ''
                   cat *.css > ./output.css
+                  cat ${ttf_EB_Garamond} >> ./output.css
                 '';
                 installPhase = ''
                   cp ./output.css $out
                 '';
               });
-            }
+            })
             {
               name = "rss.xml";
               path = (pkgs.replaceVars ./rss.xml {
