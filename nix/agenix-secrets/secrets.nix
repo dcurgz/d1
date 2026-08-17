80a40abcb1532d4d7cdda7b89bc36ae8a5e58e78
Jasper Pine <hyperberry@curz.sh>
2026-08-17 17:23:27 +0100

    nix: chore(agenix-secrets): roll tailscale keys

--- nix/agenix-secrets/secrets.nix
+++ nix/agenix-secrets/secrets.nix
@@ -23,22 +23,12 @@ with keys.ssh.hosts;
   "agenix/tailscale/hosts/piberry.age".publicKeys      = (withDefault piberry.keys);
   "agenix/tailscale/hosts/tauberry.age".publicKeys     = (withDefault tauberry.keys);
   # guests
-  "agenix/tailscale/guests/vm-claude.age".publicKeys       = (withDefault vm-claude.keys);
+  "agenix/tailscale/guests/vm-gos-update-server.age".publicKeys = (withDefault vm-gos-update-server.keys);
   "agenix/tailscale/guests/vm-immich.age".publicKeys       = (withDefault vm-immich.keys);
   "agenix/tailscale/guests/vm-jellyfin.age".publicKeys     = (withDefault vm-jellyfin.keys);
-  "agenix/tailscale/guests/vm-mc-leedl-sta.age".publicKeys = (withDefault vm-mc-leedl-sta.keys);
-  "agenix/tailscale/guests/vm-mc-leedlemon.age".publicKeys = (withDefault vm-mc-leedlemon.keys);
-  "agenix/tailscale/guests/vm-mc-wg-0.age".publicKeys      = (withDefault vm-mc-wg-0.keys);
-  "agenix/tailscale/guests/vm-mc-wg-1.age".publicKeys      = (withDefault vm-mc-wg-1.keys);
-  "agenix/tailscale/guests/vm-mc-slime-0.age".publicKeys   = (withDefault vm-mc-slime-0.keys);
-  "agenix/tailscale/guests/vm-mc-slime-1.age".publicKeys   = (withDefault vm-mc-slime-1.keys);
-  "agenix/tailscale/guests/vm-openwebui.age".publicKeys    = (withDefault vm-openwebui.keys);
-  "agenix/tailscale/guests/vm-teamspeak.age".publicKeys    = (withDefault vm-teamspeak.keys);
   "agenix/tailscale/guests/vm-trilium.age".publicKeys      = (withDefault vm-trilium.keys);
   "agenix/tailscale/guests/vm-vikunja.age".publicKeys      = (withDefault vm-vikunja.keys);
   "agenix/tailscale/guests/vx-jupiter.age".publicKeys      = (withDefault vx-jupiter.keys);
-  "agenix/tailscale/guests/vm-mb-build-aarch64.age".publicKeys = (withDefault vm-mb-build-aarch64.keys);
-  "agenix/tailscale/guests/vm-gos-update-server.age".publicKeys = (withDefault vm-gos-update-server.keys);
   # hyperberry
   "agenix/backup/restic-password.age".publicKeys     = (withDefault hyperberry.keys);
   "agenix/backup/restic-envvars.age".publicKeys      = (withDefault hyperberry.keys);
